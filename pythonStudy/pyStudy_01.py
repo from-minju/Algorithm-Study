3c469e5d6a15977a95810fe2abea8d47d7dc6ea9
Minju Kang <deco1202@naver.com>
2022-03-16 22:46:43 +0900

    Update pyStudy_01.py

--- pythonStudy/pyStudy_01.py
+++ pythonStudy/pyStudy_01.py
@@ -22,5 +22,14 @@ m = 4
 array = [ [0] * m for _ in range(n) ]
 print(array)
 
+#리스트 컴프리헨션 4
+#잘못된 방법) N x M 크기의 2차원 리스트 초기화
+n = 3
+m = 4
+array = [[0] * m] * n
+print(array)
+
+array[1][1] = 5
+print(array)
 
 
