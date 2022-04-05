a620696f2738fe7d738ebde45cf7d54242bfa296
Minju Kang <deco1202@naver.com>
2022-04-05 11:13:58 +0900

    ocajp

---
+++ javaStudy/src/n53.java
@@ -0,0 +1,20 @@
+public class n53 {
+    public static void main(String[] args) {
+        int x;
+
+        //A
+        x = 3;
+        do{
+            System.out.print("*");
+            x--;
+        } while(x >= 0);
+
+
+        //E
+        x = 0;
+        do {
+            System.out.print("*");
+        } while(x++ < 3);
+    }
+}
+
