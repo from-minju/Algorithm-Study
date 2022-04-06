244d6fd938d598a8bea7b3ce5f87ab2e01d121a4
Minju Kang <deco1202@naver.com>
2022-04-07 01:01:15 +0900

    Update n74.java

--- javaStudy/src/n74.java
+++ javaStudy/src/n74.java
@@ -1,4 +1,6 @@
+public class n74 {
 
+}
 
 class S1 {
     protected void display(int x) {
@@ -8,18 +10,16 @@ class S1 {
 
 class S2 extends S1 {
     public void display(int x, int y) {
-        this.display(x);
-        display(y);
-        super.display(y);
+        this.display(x); //S2의 display()
+        display(y); //S2의 display()
+        super.display(y); //S1의 display()
     }
     public void display(int x) {
         System.out.println("Child" + x);
     }
-}
 
-public class n74 {
-    public static void main(String[] args) {
-        S2 sobj = new S2();
-        sobj.display(10,100);
-    }
+//public static void main()이 없으므로 에러남
+//    S2 sobj = new S2();
+//        sobj.display(10,100);
+
 }
