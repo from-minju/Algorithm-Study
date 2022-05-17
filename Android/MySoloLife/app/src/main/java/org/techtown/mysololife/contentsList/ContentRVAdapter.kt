031c666f2b0f80cbcf61a5fe7246865918295810
Minju Kang <deco1202@naver.com>
2022-05-18 01:19:38 +0900

    mysololife_clonecoding_5-8_Firebase데이터 추가

--- Android/MySoloLife/app/src/main/java/org/techtown/mysololife/contentsList/ContentRVAdapter.kt
+++ Android/MySoloLife/app/src/main/java/org/techtown/mysololife/contentsList/ContentRVAdapter.kt
@@ -21,11 +21,14 @@ class ContentRVAdapter(val context : Context, val items : ArrayList<ContentModel
     var itemClick : ItemClick? = null
 
 
+    //전체 아이템들을 가져와 하나씩 넣어줌(하나의 레이이웃을 만들어줌)
+    //viewHolder객체를 생성 후 리턴
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentRVAdapter.Viewholder {
         val v = LayoutInflater.from(parent.context).inflate(R.layout.content_rv_item, parent, false)
         return Viewholder(v)
     }
 
+    //viewHolder가 재활용 될 때 사용되는 메소드
     override fun onBindViewHolder(holder: ContentRVAdapter.Viewholder, position: Int) {
 
         //item 클릭하면 화면이동할 수 있도록
@@ -37,10 +40,14 @@ class ContentRVAdapter(val context : Context, val items : ArrayList<ContentModel
         holder.bindItems(items[position])
     }
 
+    //아이템 개수가 몇개인지
     override fun getItemCount(): Int {
         return items.size
     }
 
+
+    //아이템에 있는 내용물들을 넣을 수 있게
+    //아이템 뷰를 저장하는 클래스
     inner class Viewholder(itemView : View) : RecyclerView.ViewHolder(itemView) {
         fun bindItems(item : ContentModel) {
 
@@ -57,55 +64,5 @@ class ContentRVAdapter(val context : Context, val items : ArrayList<ContentModel
 
         }
     }
-
-//    //recycler view item 클릭
-//    interface ItemClick {
-//        fun onClick(view : View, position: Int)
-//    }
-//    var itemClick : ItemClick? = null
-//
-//옴
-//    //전체 아이템들을 가져와 하나씩 넣어줌(하나의 레이이웃을 만들어줌)
-//    //viewHolder객체를 생성 후 리턴
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ContentRVAdapter.Viewholder {
-//        val v = LayoutInflater.from(parent.context).inflate(R.layout.content_rv_item, parent, false)
-//        return Viewholder(v)
-//    }
-//
-//    //viewHolder가 재활용 될 때 사용되는 메소드
-//    override fun onBindViewHolder(holder: ContentRVAdapter.Viewholder, position: Int) {
-//
-//        if(itemClick != null) {
-//            holder.itemView.setOnClickListener { v->
-//                itemClick?.onClick(v, position)
-//            }
-//        }
-//        holder.bindItems(items[position])
-//    }
-//
-//    //아이템 개수가 몇개인지
-//   override fun getItemCount(): Int {
-//        return items.size
-//    }
-//
-//    //아이템에 있는 내용물들을 넣을 수 있게
-//    //아이템 뷰를 저장하는 클래스
-//    inner class Viewholder(itemView : View) : RecyclerView.ViewHolder(itemView) {
-//
-//        fun bindItems(item : ContentModel) {
-//
-//            val contentTitle = itemView.findViewById<TextView>(R.id.textArea) //여기서의 itemView는 content_rv_item
-//            val imageViewArea = itemView.findViewById<ImageView>(R.id.imageArea)
-//
-//            contentTitle.text = item.title
-//
-//            Glide.with(context)
-//                .load(item.imageUrl)
-//                .into(imageViewArea)
-//
-//        }
-//
-//    }
-
 
 }
